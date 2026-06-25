ca89e4bbf46c7015d6c1c4ba081862f3e90a5759
Sean Crown <s.crown@rafisa.ch>
2026-06-25 15:29:24 +0200

    Javascript reingefügt, faq verbessert, css erweitert und kontakt Reworked

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
+
 const darkModeBtn = document.getElementById("darkModeBtn");
 
-darkModeBtn.addEventListener("click", function () {
+if (localStorage.getItem("darkMode") === "true") {
+    document.body.classList.add("dark");
+}
+
+darkModeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
+    localStorage.setItem(
+        "darkMode",
+        document.body.classList.contains("dark")
+    );
 });
+
+
+const faqQuestions = document.querySelectorAll(".faq-question");
+
+faqQuestions.forEach(function (question) {
+    question.addEventListener("click", function () {
+        question.parentElement.classList.toggle("active");
+    });
+});
+
+
+
